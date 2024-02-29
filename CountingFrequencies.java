8939ee328d2cb60292ba04c8fc53ff015842274a
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-01 00:26:29 +0530

    Check Pair exsists

--- CountingFrequencies.java
+++ CountingFrequencies.java
@@ -1,5 +1,7 @@
 //Array Method
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CountingFrequencies {
     public static void main(String[] args) {
@@ -22,5 +24,27 @@ public class CountingFrequencies {
             System.out.println(arr[i]+" "+ count);
         }
 
+    }public static void CountingFrequenciesHashMaps(int arr[], int size){
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for(int i =0;i<size; i++){
+            if(hm.containsKey(arr[i])){
+                hm.put(arr[i], hm.get(arr[i])+1);
+
+            }else{
+                hm.put(arr[i],1);
+            }
+        }
+     for (Map.Entry entry : hm.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
-}
+
+
+
+
+    }
+
+
+
+
