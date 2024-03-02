78f83b579f6aecfff9b4bdcede0320ef0345c3f0
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-02 19:02:32 +0530

    Add files via upload

--- HashMap/SubarraySumequalsK.java
+++ HashMap/SubarraySumequalsK.java
@@ -8,11 +8,17 @@ public class SubarraySumequalsK {
         int count =0;
         int sum =0;
         for(int i=0;i<nums.length;i++){
-           
+            sum = sum+ i;
+
+            if(hm.containsKey(sum-k)){
+                count = count + hm.get(sum-k);
+
+            }
+           hm.put(sum , hm.getOrDefault(sum ,0  )+1);
 
         
         }
-
+return count ;
 
 
 
