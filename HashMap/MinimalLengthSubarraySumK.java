c42d53396125b98d5298cc50ef8843b711b40911
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-03 16:06:03 +0530

    Add files via upload

--- HashMap/MinimalLengthSubarraySumK.java
+++ HashMap/MinimalLengthSubarraySumK.java
@@ -1,9 +1,20 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class MinimalLengthSubarraySumK {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int  n =sc.nextInt();
+        int arr []= new int[n];
+        for(int i =0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
         
-    }public static void MinimalLengthSubarraysum(int arr [], int k){
+        }
+        int k = sc.nextInt();
+        int result = MinimalLengthSubarraysum(arr, k);
+        System.out.println(result);
+    }public static int MinimalLengthSubarraysum(int arr [], int k){
         HashMap <Integer, Integer> hm = new HashMap<>();
         int count =0;
         int sum =0;
@@ -18,6 +29,7 @@ public class MinimalLengthSubarraySumK {
             minDist= Math.min(minDist, count);
             hm.put(sum , hm.getOrDefault(sum,0)+1);
         }
+        return count;
     }
 
 }
