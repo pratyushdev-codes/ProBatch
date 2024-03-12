f428a6cab191ffb0b929f9e7434033965fdde6f4
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-12 18:32:50 +0530

    Add files via upload

---
+++ Two Pointers/SquaresofSortedArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SquaresofSortedArray {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+        int arr [] = new int[n];
+        for(int i =0;i<arr.length;i++){
+            arr[i]= sc.nextInt();
+        }
+        System.out.println(squaresSortedArray(arr));
+    }public static int[] squaresSortedArray(int arr []){
+        
+        int n = arr.length;
+        int left = 0;
+        int  right=n-1;
+        int [] j = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = arr[i]*arr[i];        
+            
+        }
+        for(int i = n-1 ;i>=0 ;i--){
+            if(arr[left]> arr[right]){
+                j[i] = arr[left];
+                left++;
+            }else{
+                j[i]=arr[right];
+                right --;
+            }
+            }
+            return j;
+        }
+      
+        
+     
+    }
+    
