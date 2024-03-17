03f9435b77f06cdc5e981408d48bf9ea9006419d
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-17 13:01:39 +0530

    Add files via upload

---
+++ Binary Search/ArrangingCoins.java
@@ -0,0 +1,16 @@
+public class ArrangingCoins {
+    public static void main(String[] args) {
+        
+    }   public int arrangeCoins(int n) {
+        int start = 0;
+        int end = n;
+         while (start<=end) {
+            int mid = (end- start)+end/2;
+            //Yet to be completed S
+            
+         }
+        
+    }
+}
+    
+
