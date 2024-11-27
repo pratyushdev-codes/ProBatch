4d8496efba846bfdbbeb5e9872c43e920ba38d99
Pratyush Birole <birole.pratyush@gmail.com>
2024-11-27 12:45:11 +0530

    Add files via upload

---
+++ Hashing/countabsolutePairs.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class countabsolutePairs { 
+
+    public static void main(String[] args) {
+        
+        HashMap<Integer , Integer> hm = new HashMap<>();
+    }
+}
