c2fa86999d538ce3a1dcd73691af90f82eff5967
Pratyush Birole <birole.pratyush@gmail.com>
2024-11-27 17:33:20 +0530

    Add files via upload

--- Hashing/countabsolutePairs.java
+++ Hashing/countabsolutePairs.java
@@ -4,6 +4,13 @@ public class countabsolutePairs {
 
     public static void main(String[] args) {
         
+
         HashMap<Integer , Integer> hm = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            int complement = k - nums[i];
+
+            if*
+        }
     }
 }
